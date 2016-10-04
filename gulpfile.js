eb99632d2cb35034330fd963ecf02163ee53ce88
THBar <choreno@gmail.com>
2016-10-04 16:02:00 -0400

    initial

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            
+            baseDir: '.',  //baseDir: 'source/'
+            index: './index.html' //change the start index html file
+        }
+    })
+})
+
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watches', ['browserSync'], function () {
+    
+   
+    gulp.watch('*.html', browserSync.reload); 
+
+});
+
+
+
+gulp.task('default',['watches']);
